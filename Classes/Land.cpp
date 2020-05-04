052d8373cbaea9feb40d282b4dcab656c7d666ba
william <liyijiang3000@163.com>
2020-05-04 23:02:16 +0800

    done grass

--- Classes/Land.cpp
+++ Classes/Land.cpp
@@ -28,12 +28,12 @@ void Barren::change()
 	if (currentStatus == HAS_GRASS)
 	{
 		currentStatus = NO_GRASS;
-		//²¹³ä´úÂë
+		
 	}
 	else
 	{
 		currentStatus = HAS_GRASS;
-		//²¹³ä´úÂë
+		
 	}
 	
 }
@@ -62,12 +62,12 @@ void Fertile::change()
 	if (currentStatus == HAS_GRASS)
 	{
 		currentStatus = NO_GRASS;
-		//²¹³ä´úÂë
+		
 	}
 	else
 	{
 		currentStatus = HAS_GRASS;
-		//²¹³ä´úÂë
+		
 	}
 }
 
